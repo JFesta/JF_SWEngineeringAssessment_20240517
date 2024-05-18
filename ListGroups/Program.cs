531a479dc03e1138e90944976d7acd16c5ab69d3
j.festa <j.festa@hotmail.com>
2024-05-19 00:14:11 +0200

    Prototype: read first 10 groups

--- ListGroups/Program.cs
+++ ListGroups/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Graph;
+using System.Reflection;
+
+
+var configurationBuilder = new ConfigurationBuilder();
+configurationBuilder.AddUserSecrets(Assembly.GetExecutingAssembly(), true);
+
+var configuration = configurationBuilder.Build();
+
+var credentials = new ClientSecretCredential(configuration["AZURE_TENANT_ID"], configuration["AZURE_CLIENT_ID"], configuration["AZURE_CLIENT_SECRET"]);
+var graphClient = new GraphServiceClient(credentials);
+
+var response = await graphClient.Groups.GetAsync();
+foreach (var group in response!.Value!.Take(10))
+{
+    Console.WriteLine("{0}\t{1}", group.Id, group.DisplayName);
+}
