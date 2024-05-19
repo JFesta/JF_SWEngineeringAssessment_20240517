8d44776dc2994441e911e18f6630214c992edde8
j.festa <j.festa@hotmail.com>
2024-05-19 16:51:46 +0200

    DI and CommandLineParser setup

---
+++ ListGroups/Configs/CommandLineOptions.cs
@@ -0,0 +1,20 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListGroups.Configs;
+
+public class CommandLineOptions
+{
+    [Option("tenantId", Required = false, HelpText = "Azure TenantId")]
+    public string? TenantId { get; set; }
+
+    [Option("clientId", Required = false, HelpText = "ClientId to use for AD Client+Secret client auth")]
+    public string? ClientId { get; set; }
+
+    [Option("clientSecret", Required = false, HelpText = "Secret to use for AD Client+Secret client auth")]
+    public string? Secret { get; set; }
+}
--- ListGroups/Program.cs
+++ ListGroups/Program.cs
@@ -5,26 +5,49 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ListGroups;
 using Azure.Core;
+using ListGroups.Configs;
+using CommandLine;
+using Microsoft.Extensions.Logging;
+
+//CL arguments parsing
+CommandLine.Parser.Default.ParseArguments<CommandLineOptions>(args)
+    .WithParsed(async options =>
+     {
+         await InitAndStartApplication(options);
+     });
+
+async Task InitAndStartApplication(CommandLineOptions clOptions)
+{
+    /*
+     * Default Host creation:
+     * - Sets the Environment using the DOTNET_ENVIRONMENT env. variable
+     * - Initializes the ILogger with the Console sink
+     * - Initializes the IConfigurationBuilder with arguments, env. variables, and - if launched with the Debug launch setting - the user secrets
+    */
+    var builder = Host.CreateApplicationBuilder();
+
+    //TokenCredential initialize
+    var tokenCredentials = InitTokenCredential(clOptions);
+
+    //DI configuration
+    builder.Services.AddSingleton(tokenCredentials);
+    builder.Services.AddSingleton<GraphServiceClient>();
+    builder.Services.AddHostedService<ListGroupsHostedService>();
+
+    using IHost host = builder.Build();
+    await host.RunAsync();
+}
 
 /*
- * Default Host creation:
- * - Sets the Environment using the DOTNET_ENVIRONMENT env. variable
- * - Initializes the ILogger with the Console sink
- * - Initializes the IConfigurationBuilder with arguments, env. variables, and - if launched with the Debug launch setting - the user secrets
-*/
-var builder = Host.CreateApplicationBuilder(args);
-
-//TokenCredential initialize
-var tokenCredentials = InitTokenCredential();
-
-//DI configuration
-builder.Services.AddSingleton(tokenCredentials);
-builder.Services.AddHostedService<ListGroupsHostedService>();
-
-using IHost host = builder.Build();
-await host.RunAsync();
-
-TokenCredential InitTokenCredential()
+ * Credentials info are selected from (from the highest to the lowest priority):
+ * - Console args
+ * - User Secrets (only if Environment = Development)
+ * - Environment Variables
+ * - Other mechanisms, e.g. Managed Identity (check the DefaultAzureCredential class' doc: https://learn.microsoft.com/en-us/dotnet/api/azure.identity.defaultazurecredential?view=azure-dotnet)
+ */
+TokenCredential InitTokenCredential(CommandLineOptions clOptions)
 {
-    return new DefaultAzureCredential();
+    return (!string.IsNullOrWhiteSpace(clOptions.TenantId) && !string.IsNullOrWhiteSpace(clOptions.ClientId) && !string.IsNullOrWhiteSpace(clOptions.Secret))
+        ? new ClientSecretCredential(clOptions.TenantId, clOptions.ClientId, clOptions.Secret)
+        : new DefaultAzureCredential();
 }
